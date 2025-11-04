0f062e77f1a1b962495e84cd07739019db56fef4
nousScrates <phelouma@gmail.com>
2025-11-04 12:04:31 +0300

    First Frontend Commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gradient-to-br from-sky-500 to-indigo-700 text-white text-4xl font-bold">
+      Hello, Tailwind + Vite + TypeScript 🎨
+    </div>
+  );
+}
+
+export default App;
